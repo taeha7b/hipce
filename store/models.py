84f500a17cc5a938fb597a552df7617d7fafeee2
Taehyun Lee <0417taehyun@gmail.com>
2020-08-19 20:46:31 +0900

    Add: store App 모델 추가 - store App 생성 - store App models.py 추가

---
+++ store/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Store(models.Model):
+    name         = models.CharField(max_length = 200)
+    address      = models.CharField(max_length = 256)
+    business_day = models.CharField(max_length = 50, blank = True, null = True)
+    opening_hour = models.CharField(max_length = 50)
+    contact      = models.CharField(max_length = 50)
