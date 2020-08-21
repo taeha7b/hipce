c771e7de7576215d1381e037c562ba94cbd25a6c
Taeha Kim <taeha7b@gmail.com>
2020-08-21 17:49:37 +0900

    Modify: user app 수정 - validateion.py 메세지를 문자열로 변경

--- user/validation.py
+++ user/validation.py
@@ -4,24 +4,24 @@ import re
 def validate_phone(value):
     regex = re.compile(r'(\d{3}).*(\d{4}).*(\d{4})')
     if not regex.match(value):
-        raise ValidationError(message= validate_phone)
+        raise ValidationError(message= 'validate_phone')
 
 def validate_email(value):
     regex = re.compile(r'^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$')
     if not regex.match(value):
-        raise ValidationError(message = validate_email)
+        raise ValidationError(message = 'validate_email')
 
 def validate_account(value):
     regex = re.compile(r'^[a-zA-Z0-9]{4,16}$')
     if not regex.match(value):
-        raise ValidationError(message = validate_account)
+        raise ValidationError(message = 'validate_account')
 
 def validate_birthday(value):
     regex = re.compile(r'^[0-9]{8}')
     if not regex.match(value):
-        raise ValidationError(message = validate_birthday)
+        raise ValidationError(message = 'validate_birthday')
         
 def validate_name(value):
     regex = re.compile(r'^[가-힣a-zA-Z]+$')
     if not regex.match(value):
-        raise ValidationError(message = validate_name)
+        raise ValidationError(message = 'validate_name')
