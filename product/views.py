d42e3a60937ec9f0839e426f80e37e3127c1fe39
Taehyun Lee <0417taehyun@gmail.com>
2020-08-26 01:40:30 +0900

    Add: 전체컬러를 보여주는 엔드포인트 및 컬러에 따른 일부 제품을 보여주는 기능을 제품 리스트 엔드포인트에 추가 - 기존 카테고리를 기준으로 제품 리스트를 보여주는 엔드포인트에 컬러 필터 옵션을 추가 - 모든 컬러 정보를 보내주기 위한 엔드포인트 추가

--- product/views.py
+++ product/views.py
@@ -1,9 +1,10 @@
 import json
 
-from django.views import View
-from django.http  import JsonResponse
+from django.views     import View
+from django.http      import JsonResponse
+from django.db.models import Q
 
-from .models      import Category, Product, Color
+from .models          import Category, Product, Color
 
 class CategoryView(View):
     def get(self, request):
@@ -20,19 +21,24 @@ class CategoryView(View):
 
 class ProductsView(View):
     def get(self, request):
-        ''' 만든 이: 이태현
-        최종 수정: 2020. 08. 25.
+        '''만든 이: 이태현
+        최종 수정: 2020. 08. 26.
         정보: Shop 페이지 카테고리 별 상품 이름 및 이미지 정보를 보내주는 GET방식 함수
         설명
-        - 카테고리를 기준으로 필터해주기 위해 쿼리 스트링 사용
+        - 카테고리를 기준으로 필터해주기 위해 get을 통한 쿼리 스트링 사용
+        - 여러 컬러를 필터해주기 위해 getlist을 통한 쿼리 스트링 사용
+        - 카테고리의 값에 아무 것도 들어가지 않았을 경우(Default) lip 카테고리 상품을 보여줌
+        - 컬러의 값에 아무 것도 들어가지 않았을 경우(Default) 모든 제품을 보여주기 위해 if를 사용
+          color_list에 값이 들어간 경우 그 값을 가진 상품 정보들만 필터
         - 프론트 엔드에 정보만 전달하면 되고 키, 밸류로 값을 사용하기 때문에 values 쿼리셋 사용'''
-        category_name = request.GET.get('category', None)
-        colors_list   = request.GET.getlist('color', None)
-        category_id   = Category.objects.get(name = category_name).id
-        colors = Color.objects.filter('co')
-        products      = Product.objects.filter(category = category_id).values(
-            'name', 'main_image', 'sub_image', 'price', 'tag'
+        category_name = request.GET.get('category', 'lip')
+        color_list    = request.GET.getlist('color', None)
+        products      = Product.objects.prefetch_related('color').filter(
+            category__name = category_name
         )
+        if color_list:
+            products = products.filter(color__name__in = color_list)
+        products = products.values('name', 'main_image', 'sub_image', 'price', 'tag')
         return JsonResponse(
             {'products':list(products)},
             status = 200
@@ -40,7 +46,7 @@ class ProductsView(View):
 
 class ProductView(View):
     def get(self, request, id):
-        ''' 만든 이: 이태현
+        '''만든 이: 이태현
         최종 수정: 2020. 08. 25.
         정보: Shop 페이지 각 상품 상세 페이지 정보를 보내주는 GET방식 함수
         설명
@@ -53,22 +59,15 @@ class ProductView(View):
             status = 200
         )
 
-class ColorView(View):
+class ColorsView(View):
     def get(self, request):
+        '''만든 이: 이태현
+        최종 수정: 2020. 08. 25.
+        정보: Shop 페이지 컬러 리스트를 보여주는 GET방식 함수
+        설명
+        - 프론트 엔드에 정보만 전달하면 되고 키, 밸류로 값을 사용하기 때문에 values 쿼리셋 사용'''
         colors = Color.objects.values('name')
         return JsonResponse(
             {'colors':list(colors)},
-            status = 200
-        )
-
-class ColorFilterView(View):
-    def get(self, request):
-        color_name = request.GET.get('color', None)
-        color      = Color.objects.get(name = color_name)
-        products = color.product_set.values(
-            'name', 'main_image', 'sub_image', 'price', 'tag'
-        )
-        return JsonResponse(
-            {'products':list(products)},
             status = 200
         )
