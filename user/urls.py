7de90c4880489466e49295962d5ee9586f0a0721
Taeha Kim <taeha7b@gmail.com>
2020-08-25 16:36:26 +0900

    Modify: url에 배송지 추가

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import SignUp
-from .views import SignIn
+from .views import (
+    SignUp, 
+    SignIn,
+    DeliveryAddress,
+)
 
 urlpatterns = [
     path('/signup', SignUp.as_view()),
     path('/signin', SignIn.as_view()),
+    path('/delivery_address', DeliveryAddress.as_view()),
 ]
