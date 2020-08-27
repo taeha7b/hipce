a19515271a0ea118d56ef83042d702937453222b
Taeha Kim <taeha7b@gmail.com>
2020-08-27 12:26:37 +0900

    Add: reviews app에서 리뷰기능 구현

--- reviews/views.py
+++ reviews/views.py
@@ -12,17 +12,15 @@ class Review(View):
     def get(self, request):
         try:
             review = list(Review.objects.values(), ReviewImage.objects.image())
-            return JsonResponse({'Review':review},  status = 200)
+            return JsonResponse({'Review':review}, status = 200)
         
         except json.decoder.JSONDecodeError:
             return JsonResponse({"MESSAGE": "JSONDecodeError"}, status = 401)
-
 
     @login_confirm
     def post(self, request):
         try:
             data = json.loads(request.body)
-
             review = Review(
                 user     = request.account,
                 product  = Product.objects.get(name = data['name']),
@@ -37,40 +35,46 @@ class Review(View):
             )
             review_iamge.save()
             return JsonResponse({"MESSAGE": "SUCCESS"}, status = 200)
-        except :
-            pass
-
-    # @login_confirm
-    # def put(self, request):
-    #     try:
-    #         data = json.loads(request.body)
-
-    #         review = Review(
-    #             user     = request.account,
-    #             product  = Product.objects.get(name = data['name']),
-    #             content  = data['content'],
-    #             score    = data['score']
-    #         )
-    #         review.save()
             
-    #         review_iamge = ReviewImage(
-    #             review  = review,
-    #             image   = data['image']
-    #         )
-    #         review_iamge.save()
- 
-    #     except KeyError:
-    #         return JsonResponse({"MESSAGE": "KEY_ERROR"}, status = 400)
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status = 400)
+
+    @login_confirm
+    def put(self, request):
+        try:
+            data = json.loads(request.body)
+            user_id = request.account.id
+            review_id = data['review_id']
+            modification_review = Review.objects.get(
+                user = user_id, 
+                id = review_id
+            )
+            modification_review.content = data['content']
+            modification_review.save()
+
+            if not data['image'] == '': 
+                review_image_id = data['review_image_id']
+
+                modification_review_image = ReviewImage.objects.get(
+                    review = review_id, 
+                    id = review_image_id
+                )
+                modification_review_image = data['image']
+                modification_review_image.save()
+            pass
+            
+        except KeyError:
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status = 400)
         
-    #     except json.decoder.JSONDecodeError:
-    #         return JsonResponse({"MESSAGE": "JSONDecodeError"}, status = 401)
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({"MESSAGE": "JSONDecodeError"}, status = 401)
     
     @login_confirm
     def delete(self, request):
         try:
             data = json.loads(request.body)
             Review.objects.get(id = data['id']).delete()
-            return JsonResponse({'MESSAGE':"Review was deleted"}, status = 200)
+            return JsonResponse({"MESSAGE": "Review was deleted"}, status = 200)
 
         except KeyError:
             return JsonResponse({"MESSAGE": "KEY_ERROR"}, status = 400)
